409aee6dfd59b1c67a20e8991e9a4730bf4db1a4
Maxime Beaudoin <maxime.beaudoin@ellipse-synergie.com>
2015-04-15 13:41:38 -0400

    Update phpdoc

--- src/Contracts/Response.php
+++ src/Contracts/Response.php
@@ -6,7 +6,12 @@ use League\Fractal\Pagination\Cursor;
 
 /**
  * Interface Response
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
  * @package EllipseSynergie\ApiResponse\Contracts
+ * @author Maxime Beaudoin <maxime.beaudoin@ellipse-synergie.com>
  */
 interface Response
 {
